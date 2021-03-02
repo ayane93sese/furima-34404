b75de1a31a542336ed714984456f36a9e0646e7b
ayane93sese <koro.115ayapi@gmail.com>
2021-03-02 16:13:30 +0900

    バリテーション・エラーメッセージの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence:true
+  validates :family_name, presence:true, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "Full-width characters"}
+  validates :first_name, presence:true
+  validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "Full-width characters"}
+  validates :family_name_ruby, presence:true, format: {with: /\A[ァ-ヶ]+\z/, message: "Full-width katakana characters"}
+  validates :first_name_ruby, presence:true, format: {with: /\A[ァ-ヶ]+\z/, message: "Full-width katakana characters"}
+  validates :birth, presence:true
+  validates :password, format: {with: /\A[a-z\d]+\z/, message: "Include both letters and numbers"}
+
 end
