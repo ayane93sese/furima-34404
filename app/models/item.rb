0c2cbb545b1be5a60f918426dba0f1202ef8c7d5
ayane93sese <koro.115ayapi@gmail.com>
2021-03-03 16:56:05 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :payment
+  belongs_to :area
+  belongs_to :days
+
   belongs_to :user
   has_one_attached :image
 end
