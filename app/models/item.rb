d2d0643031280b11e05f1c417270de931cb30cdb
ayane93sese <koro.115ayapi@gmail.com>
2021-03-03 19:00:51 +0900

    ActiveHashのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,21 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  validates :title, :text, presence: true
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :payment_id
+    validates :area_id
+    validates :days_id
+  end
+
 end
+
+#バリテーションまだ
+#| price       | integer    | null:false                     |
+#| user        | references | null :false, foreign_key: true |
+#バリテーション謎
+#image 一応has_one_attachedはやったけどお果たしてこれでいいのか謎のため
