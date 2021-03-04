4f35d46f4b9ccb951089d74cd1f7a44c2ca3fc4d
ayane93sese <koro.115ayapi@gmail.com>
2021-03-04 17:02:27 +0900

    FactoryBot・テストコード修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     title {"test"}
     text {Faker::Lorem.sentence}
-    category_id {"2"}
-    status_id {"2"}
-    payment_id {"2"}
-    area_id {"2"}
-    days_id {"2"}
-    price {"500"}
+    category_id {2}
+    status_id {2}
+    payment_id {2}
+    area_id {2}
+    days_id {2}
+    price {500}
     association :user
 
     after(:build) do |item|
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -19,23 +19,23 @@ RSpec.describe Item, type: :model do
         expect(@item).to be_valid
       end
       it "category_idが1以外で選択されていれば登録できる" do
-        @item.category_id = "2"
+        @item.category_id = 2
         expect(@item).to be_valid
       end
       it "status_idが1以外で選択されていれば登録できる" do
-        @item.status_id = "2"
+        @item.status_id = 2
         expect(@item).to be_valid
       end
       it "payment_idが1以外で選択されていれば登録できる" do
-        @item.payment_id = "2"
+        @item.payment_id = 2
         expect(@item).to be_valid
       end
       it "area_idが1以外で選択されていれば登録できる" do
-        @item.area_id = "2"
+        @item.area_id = 2
         expect(@item).to be_valid
       end
       it "days_idが1以外で選択されていれば登録できる" do
-        @item.days_id = "2"
+        @item.days_id = 2
         expect(@item).to be_valid
       end
       it "priceが300〜9999999の間で存在すれば登録できる" do
@@ -61,42 +61,47 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Text can't be blank")
       end
       it "category_idが1では登録できない" do
-        @item.category_id = "1"
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category Select")
       end
       it "status_idが1では登録できない" do
-        @item.status_id = "1"
+        @item.status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Status Select")
       end
       it "payment_idが1では登録できない" do
-        @item.payment_id = "1"
+        @item.payment_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Payment Select")
       end
       it "area_idが1では登録できない" do
-        @item.area_id = "1"
+        @item.area_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Area Select")
       end
       it "days_idが1では登録できない" do
-        @item.days_id = "1"
+        @item.days_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Days Select")
       end
       it "priceが300以上でないと登録できない" do
-        @item.price = "299"
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include("Price Out of setting range")
       end
       it "priceが9999999より大きいと登録できない" do
-        @item.price = "10000000"
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price Out of setting range")
       end
       it "priceが全角だと登録できない" do
         @item.price = "３００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price Out of setting range")
+      end
+      it "priceに数字以外が入力されていると登録できない" do
+        @item.price = "test"
         @item.valid?
         expect(@item.errors.full_messages).to include("Price Out of setting range")
       end
